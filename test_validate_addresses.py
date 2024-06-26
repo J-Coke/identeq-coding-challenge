7e6bee020aefdb59e28c5bb51de6ebdda4c4989d
Alexander Coker <alexandercoker@Alexanders-MacBook-Pro.local>
2024-06-26 17:59:07 +0100

    Process abp data handles multiple addresses

--- test_validate_addresses.py
+++ test_validate_addresses.py
@@ -10,7 +10,17 @@ def test_process_abp_data_can_process_one_address():
         'POSTCODE': ['CB2 1AB'],
         'STREET_NAME': ['REGENT STREET']
     })
-    print(type(input_data))
     output = process_abp_data(input_data)
     expected_output = {'CB2 1AB':['REGENT STREET']}
+    assert output == expected_output
+
+def test_process_abp_data_can_process_multiple_addresses():
+    input_data = pd.DataFrame({
+        'SINGLE_LINE_ADDRESS': ["AAA", "BBB", "CCC"],
+        'POSTCODE': ['CB2 1AB', 'CB2 1AB', 'CB3 5DF'],
+        'STREET_NAME': ['REGENT STREET', 'NEW STREET', 'REGENT STREET']
+    })
+    output = process_abp_data(input_data)
+    expected_output = {'CB2 1AB':['REGENT STREET', 'NEW STREET'],
+                       'CB3 5DF':['REGENT STREET']}
     assert output == expected_output
--- validate_addresses.py
+++ validate_addresses.py
@@ -4,5 +4,8 @@ import pandas as pd
 def process_abp_data(abp_input_table):
     abp_data_by_postcode = {}
     for index, row in abp_input_table.iterrows():
-        abp_data_by_postcode[row['POSTCODE']] = [row['STREET_NAME']]
+        if row['POSTCODE'] not in abp_data_by_postcode:
+            abp_data_by_postcode[row['POSTCODE']] = [row['STREET_NAME']]
+        else:
+            abp_data_by_postcode[row['POSTCODE']].append(row['STREET_NAME'])
     return abp_data_by_postcode
