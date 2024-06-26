c2a2edb9b349a7778416921febd8291ee73ce48c
Alexander Coker <alexandercoker@Alexanders-MacBook-Pro.local>
2024-06-26 18:47:29 +0100

    Process input row converts to upper case

--- test_validate_addresses.py
+++ test_validate_addresses.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from validate_addresses import process_abp_data
+from validate_addresses import process_abp_data, process_input_row
 
 
 def test_process_abp_data_can_process_one_address():
@@ -23,4 +23,18 @@ def test_process_abp_data_can_process_multiple_addresses():
     output = process_abp_data(input_data)
     expected_output = {'CB2 1AB':['REGENT STREET', 'NEW STREET'],
                        'CB3 5DF':['REGENT STREET']}
+    assert output == expected_output
+
+def test_process_input_row_converts_to_upper_case():
+    input_data = pd.DataFrame({
+
+        'Address_Line_1': ['Trough Field Supply'],
+        'Address_Line_2': ['Delgate Bank'],
+        'Address_Line_3': ['Weston Hills'],
+        'Address_Line_4': ['SPALDING'],
+        'Address_Line_5': [''],
+        'Postcode': ['PE12 6DW']
+    })
+    output = process_input_row('Trough Field Supply', 'Delgate Bank', 'Weston Hills', 'PE12 6DW')
+    expected_output = ('TROUGH FIELD SUPPLY DELGATE BANK WESTON HILLS', 'PE12 6DW')
     assert output == expected_output
--- validate_addresses.py
+++ validate_addresses.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
 
-def process_abp_data(abp_input_table):
-    abp_data_by_postcode = {}
-    for index, row in abp_input_table.iterrows():
-        if row['POSTCODE'] not in abp_data_by_postcode:
-            abp_data_by_postcode[row['POSTCODE']] = [row['STREET_NAME']]
+def process_abp_data(abp_table):
+    valid_addresses_by_postcode = {}
+    for index, row in abp_table.iterrows():
+        if row['POSTCODE'] not in valid_addresses_by_postcode:
+            valid_addresses_by_postcode[row['POSTCODE']] = [row['STREET_NAME']]
         else:
-            abp_data_by_postcode[row['POSTCODE']].append(row['STREET_NAME'])
-    return abp_data_by_postcode
+            valid_addresses_by_postcode[row['POSTCODE']].append(row['STREET_NAME'])
+    return valid_addresses_by_postcode
+
+def process_input_row(column_1, column_2, column_3, postcode):
+    return (" ".join([column_1.upper(), column_2.upper(), column_3.upper()]), postcode)
